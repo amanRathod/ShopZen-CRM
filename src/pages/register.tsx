264474e789af44f915c27c911250816eb5213de2
amanRathod <aksrathod07@gmail.com>
2023-04-26 13:56:31 +0530

    create register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import Form from '@components/form';
+import AuthPage from '@layouts/page/AuthPage';
+import * as y from 'yup';
+import React from 'react';
+import Input from '@elements/form/Input';
+import LinkedItem from '@elements/LinkedItem';
+
+const registerSchema = y.object().shape({
+  firstName: y.string().required('First Name is required'),
+  lastName: y.string().required('Last Name is required'),
+  email: y.string().email('Invalid email').required('Email is required'),
+  password: y.string().required('Password is required'),
+  confirmPassword: y
+    .string()
+    .required('Confirm Password is required')
+    .oneOf([y.ref('password')], 'Passwords must match'),
+});
 
 const Register = () => {
-   return (
-     <div>Register!</div>
-   )
- }
- 
- export default Register;
+  return (
+    <AuthPage pageTitle="Login" title="Register your account">
+      <Form
+        schema={registerSchema}
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+        }}
+        submitButton={{ title: 'Sign Up' }}
+      >
+        <Input
+          name="firstName"
+          label="First Name"
+          type="text"
+          required
+          autoComplete="username"
+        />
+
+        <Input
+          name="lastName"
+          label="Last Name"
+          type="text"
+          required
+          autoComplete="username"
+        />
+
+        <Input
+          name="email"
+          label="Email"
+          type="email"
+          required
+          autoComplete="username"
+        />
+
+        <Input
+          name="password"
+          label="Password"
+          type="password"
+          required
+          autoComplete="current-password"
+        />
+
+        <Input
+          name="password"
+          label="Confirm Password"
+          type="password"
+          required
+          autoComplete="current-password"
+        />
+      </Form>
+    </AuthPage>
+  );
+};
+
+export default Register;
