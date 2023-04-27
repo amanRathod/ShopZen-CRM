892d57e2ed52a496bd618d7f1d51a935a2f53ee3
amanRathod <aksrathod07@gmail.com>
2023-04-27 08:04:36 +0530

    create customer axios instance

---
+++ src/lib/axios.ts
@@ -0,0 +1,28 @@
+import Axios, { AxiosRequestConfig } from "axios";
+import { storage } from "@utils/storage";
+
+const axios = Axios.create({
+  baseURL: process.env.SERVER_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axios.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    return Promise.reject(error.response.data);
+  }
+);
+
+axios.interceptors.request.use((config: AxiosRequestConfig) => {
+  const token = storage.getToken();
+
+  if (token) {
+    config!.headers!.Authorization = token ? `Bearer ${token}` : "";
+  }
+
+  return config;
+});
+
+export default axios;
