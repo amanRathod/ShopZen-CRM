09706b152418e007136f6009cd31e445dd31307a
amanRathod <aksrathod07@gmail.com>
2023-05-16 20:13:54 +0530

    improve pagination button

--- src/common/components/pagination/index.tsx
+++ src/common/components/pagination/index.tsx
@@ -35,19 +35,19 @@ const Pagination: React.FC<Props> = ({
         pageRangeDisplayed={5}
         onPageChange={handlePageChange}
         containerClassName={
-          'flex items-center justify-center space-x-4 text-gray-500 '
+          'flex flex-wrap items-center justify-center space-x-1  sm:space-x-4 text-gray-500'
         }
         pageClassName={
-          'inline-block mx-2 rounded-full px-3 py-2 hover:bg-gray-200'
+          'inline-block mx-2 rounded-full px-3 py-2 hover:bg-gray-200 border border-gray-300 shadow-md shadow-primary-300/30'
         }
         pageLinkClassName={
           'block w-full h-full outline-none focus:outline-none'
         }
         previousClassName={
-          'inline-block mx-2 rounded-full px-3 py-2 hover:bg-gray-200'
+          'inline-block mx-2 rounded-lg px-3 py-2 hover:bg-gray-200'
         }
         nextClassName={
-          'inline-block mx-2 rounded-full px-3 py-2 hover:bg-gray-200'
+          'inline-block mx-2 rounded-lg px-3 py-2 hover:bg-gray-200'
         }
         previousLinkClassName={
           'block w-full h-full outline-none focus:outline-none'
@@ -55,14 +55,14 @@ const Pagination: React.FC<Props> = ({
         nextLinkClassName={
           'block w-full h-full outline-none focus:outline-none'
         }
-        activeClassName={'bg-blue-500 text-white'}
+        activeClassName={'bg-blue-600 text-white border'}
         breakClassName={'text-gray-500 inline-block mx-2'}
         disabledClassName={'text-gray-400 inline-block mx-2'}
         previousLabel={'← Previous'}
         nextLabel={'Next →'}
       />
       <select
-        className="py-2 px-3 pr-4 block border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 mt-2"
+        className="py-2 px-4 pr-4 block border-gray-200 rounded-md text-sm"
         value={pageSize}
         onChange={handlePageSizeChange}
       >
